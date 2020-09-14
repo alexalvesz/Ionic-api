341af40d412fe38d95fd287c0336856a8f90a0d0
alexalvesz <alex_alves010@hotmail.com>
2020-09-14 14:01:31 -0300

    Aula-08

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -1,34 +1,34 @@
 import { Injectable } from '@angular/core';
 
-
-// Importar as bibliotecas assincronas do Angualar.abs
+// Importar as bilbiotecas assíncronas do Angular
 import { Observable } from 'rxjs';
 
-
-
-// Importar as bibliotecas HTTP 
+// Importar as bilbiotecas HTTP
 import { HttpClient } from '@angular/common/http';
+
 @Injectable({
   providedIn: 'root'
 })
 export class UsersService {
-  // URL "base" da API]
-  private apiurl = 'http://localhost:8888/api'
 
+  // URL "base" da API
+  private apiurl = 'http://localhost:8888/api';
 
   constructor(
-    private http: HttpClient 
-    
+    private http: HttpClient
   ) { }
-  //Método para obter todos os usuários da API
-    getUsers(): Observable<any> {
-      
-      // Faz o GET na API
-      return this.http.get(this.apiurl);
-    }
 
-// Método para listar um usuario especifico pelo Id
-getUser(id:string): Observable<any> {
-  return this.http.get('${this.apiurl}?id=${id}');
-}
+  // Método para obter todos os usuários da API
+  getUsers(): Observable<any> {
+
+    // Faz o GET na API
+    return this.http.get(this.apiurl);
+  }
+
+  // Método para listar um usuário específico pelo Id
+  getUser(id: string): Observable<any> {
+
+    // Consulta à API no formato "http://localhost:8888/api?id={id}"
+    return this.http.get(`${this.apiurl}?id=${id}`);
+  }
 }
